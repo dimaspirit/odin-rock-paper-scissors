93e8f6c4f3b4a617795f3f0f9ae8096230c20349
Dmytro Lobanov <dspwww@gmail.com>
2025-04-01 15:49:29 +0100

    feat: add getComputerChoice function

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 console.log('Hello, world!')
+
+function getComputerChoice() {
+  const random = Math.floor(Math.random() * 3);
+  return ['rock', 'paper', 'scissors'][random];
+}
+
+console.log('Computer choice:', getComputerChoice());
