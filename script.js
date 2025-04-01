79d7a2933536d5a2673298f7d92d1fd2ea13381a
Dmytro Lobanov <dspwww@gmail.com>
2025-04-01 16:03:24 +0100

    feat: Add playRound function

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 console.log('Hello, world!')
 
+let computerScore = 0;
+let humanScore = 0;
+
 function getComputerChoice() {
   const random = Math.floor(Math.random() * 3);
   return ['rock', 'paper', 'scissors'][random];
@@ -10,5 +13,22 @@ function getHumanChoice() {
   return choice;
 }
 
-console.log('Computer choice:', getComputerChoice());
-console.log('Human choice:', getHumanChoice());
+function playRound(humanChoice, computerChoice) {
+  if (humanChoice === computerChoice) {
+    console.log('It\'s a tie!');
+  } else if (
+    (humanChoice === 'rock' && computerChoice === 'scissors') ||
+    (humanChoice === 'paper' && computerChoice === 'rock') ||
+    (humanChoice === 'scissors' && computerChoice === 'paper')
+  ) {
+    console.log('You win!');
+    humanScore++;
+  } else {
+    console.log('You lose!');
+    computerScore++;
+  }
+
+  console.log(`Scores - Human: ${humanScore}, Computer: ${computerScore}`);
+}
+
+playRound(getComputerChoice(), getHumanChoice());
